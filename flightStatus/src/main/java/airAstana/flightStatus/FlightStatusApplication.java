097a71c260bb292a8fb282baa5adf520dbf58b15
alisher <alisher.sharipov2@Mail.ru>
2024-06-19 17:09:26 +0500

    Initial Push

---
+++ flightStatus/src/main/java/airAstana/flightStatus/FlightStatusApplication.java
@@ -0,0 +1,13 @@
+package airAstana.flightStatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightStatusApplication.class, args);
+	}
+
+}
---
+++ flightStatus/src/test/java/airAstana/flightStatus/FlightStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package airAstana.flightStatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
